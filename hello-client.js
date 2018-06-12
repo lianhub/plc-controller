85f4e087e9c9b786b1d43da7cdb4155b41fbe0bf
jerry <jerry.lian@gmail.com>
2018-06-12 16:18:50 -0400

    take std.out

--- hello-client.js
+++ hello-client.js
@@ -21,27 +21,33 @@ console.log("Sending request", 0, '…');
 
 const syncMsgBtn = document.getElementById('sendSyncMsgBtn')
 syncMsgBtn.addEventListener('click', function () {
-    document.getElementById('syncReply').innerHTML = "sync"
     requester.send(JSON.stringify({type: 'start', file: 'ecat'}) )
     cmd = "start"
 })
 
 const asyncMsgBtn = document.getElementById('sendAsyncMsgBtn')
 asyncMsgBtn.addEventListener('click', function () {
-    document.getElementById('asyncReply').innerHTML = "async"
     requester.send(JSON.stringify({type: 'stop', file: 'ecat'}) )
     cmd = "stop"
 })
 
 const loginBtn = document.getElementById('loginBtn')
 loginBtn.addEventListener('click', function () {
-    document.getElementById('login').innerHTML = "oosync"
     requester.send(JSON.stringify({type: 'estart', file: 'ecat'}) )
     cmd = "estart"
 })
 const logoutBtn = document.getElementById('logoutBtn')
 logoutBtn.addEventListener('click', function () {
-    document.getElementById('logout').innerHTML = "oo  sync"
     requester.send(JSON.stringify({type: 'estop', file: 'ecat'}) )
     cmd = "estop"
 })
+//**************************************************************************
+var suber = zmq.socket('sub');// socket to talk to server
+suber.subscribe("")
+suber.on("message", function(reply) {
+    //console.log("pub/sub reply", ": ", reply.toString());
+    document.getElementById('statusReply').innerHTML = cmd + ": " + reply.toString()
+    //suber.close();    //process.exit(0);
+});
+//suber.connect("tcp://localhost:5565");
+suber.connect("tcp://10.0.0.2:5565");
